2f77852dbf137686f4f97c28c16153a265c3b82f
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-10 13:40:33 +0300

    Добавлен читатель символов, который отвечает за обрезание первого символа из строки, а так же позволяет получить первый символ

---
+++ characterreader.py
@@ -0,0 +1,33 @@
+from typing import NoReturn
+
+
+class CharacterReaderException(Exception):
+    pass
+
+
+class ArrayOverflowError(CharacterReaderException):
+    pass
+
+
+class CharacterReader:
+    def __init__(self, code: str) -> None:
+        self.__code = code
+        self.__selected_symbol = self.__code[0]
+
+    @property
+    def selected_symbol(self) -> str:
+        """
+        :return: Возвращает выбранный символ
+        """
+        return self.__selected_symbol
+
+    def trip_first_character(self) -> NoReturn:
+        """
+        Обрезает первый символ из всей строки
+        :return:
+        """
+        if len(self.__code) > 0:
+            self.__selected_symbol = self.__code[0]
+            self.__code = self.__code[1:]
+        else:
+            raise ArrayOverflowError("An array overflow has occurred")
