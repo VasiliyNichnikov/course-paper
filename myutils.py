7278be4328d1f52e38ba91f248525e7bc5f2de3b
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-12 09:39:57 +0300

    Добавлен класс, реализующий основные методы, которые используются в разных классах

---
+++ myutils.py
@@ -0,0 +1,8 @@
+from buffer import Buffer
+from characterreader import CharacterReader
+
+
+def reading_next_character_and_cleaning_buffer(buffer: Buffer, reader: CharacterReader) -> None:
+    buffer.clear()
+    buffer.add(reader.selected_symbol)
+    reader.trip_first_character()
