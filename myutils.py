9cf435de23139b177306e7d0547d31017c720096
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-13 16:18:01 +0300

    Добавлен новый метод в myutils.py

--- myutils.py
+++ myutils.py
@@ -6,3 +6,8 @@ def reading_next_character_and_cleaning_buffer(buffer: Buffer, reader: Character
     buffer.clear()
     buffer.add(reader.selected_symbol)
     reader.trip_first_character()
+
+
+def reading_next_character(buffer: Buffer, reader: CharacterReader) -> None:
+    buffer.add(reader.selected_symbol)
+    reader.trip_first_character()
