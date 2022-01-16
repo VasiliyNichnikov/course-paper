7fb39ece101918f659f925408d5dad61c3820d80
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-16 09:38:07 +0300

    Подправлен ConditionI

--- conditions/conditioni.py
+++ conditions/conditioni.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from buffer import Buffer
 from characterreader import CharacterReader
 from checkingsymbol import CheckingSymbol
@@ -9,7 +7,6 @@ from conditions.typescondition import TypesCondition
 from myutils import reading_next_character
 from tokens.typesoftokentables import TypesOfTokenTables
 from tokens.workingwithtoken import WorkingWithToken
-from transitions.transitionparent import TransitionParent
 
 
 class ConditionI(ConditionParent):
@@ -19,19 +16,18 @@ class ConditionI(ConditionParent):
     def action(self) -> None:
         self.__cleaning_from_code()
         self.__search_token_in_service_table()
-        super(ConditionI, self).action()
+
+        if self._token.z != -1:
+            self._token.writing_to_token_file(0, self._token.z)
+        else:
+            self._token.writing_token_to_table(TypesOfTokenTables.IDS)
+        self._condition.now = TypesCondition.H
 
     def __cleaning_from_code(self) -> None:
         checking_symbol = CheckingSymbol()
         while checking_symbol.is_value_letter(self._reader.selected_symbol) or \
                 checking_symbol.is_value_number(self._reader.selected_symbol):
             reading_next_character(self._buffer, self._reader)
-
-        if self._token.z != 0:
-            self._token.writing_to_token_file(0, self._token.z)
-        else:
-            self._token.writing_token_to_table(TypesOfTokenTables.IDS)
-        self._condition.now = TypesCondition.H
 
     def __search_token_in_service_table(self) -> None:
         self._token.find_token_in_selected_table(TypesOfTokenTables.SERVICE)
