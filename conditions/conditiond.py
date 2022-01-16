544834d09c03dd2fe0667866a70af867e1a77a42
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-16 09:10:04 +0300

    Доработано состояние conditionD

--- conditions/conditiond.py
+++ conditions/conditiond.py
@@ -2,15 +2,29 @@ from typing import List
 
 from buffer import Buffer
 from characterreader import CharacterReader
+from checkingsymbol import CheckingSymbol
 from conditions.condition import Condition
 from conditions.conditionparent import ConditionParent
+from conditions.typescondition import TypesCondition
+from tokens.typesoftokentables import TypesOfTokenTables
 from tokens.workingwithtoken import WorkingWithToken
 from transitions.transitionparent import TransitionParent
 
 
 class ConditionD(ConditionParent):
-    def __init__(self, reader: CharacterReader, buffer: Buffer, token: WorkingWithToken, condition: Condition) ->None:
+    def __init__(self, reader: CharacterReader, buffer: Buffer, token: WorkingWithToken, condition: Condition) -> None:
         super().__init__(reader, buffer, token, condition)
-        
+
     def action(self, transitions: List[TransitionParent]) -> None:
-        super(ConditionD, self).action(transitions)
+        checking_symbol = CheckingSymbol()
+        if self._reader.selected_symbol in ['H', 'h']:
+            self._reader.trip_first_character()
+            self._condition.now = TypesCondition.HX
+        elif checking_symbol.check_hex(self._reader.selected_symbol):
+            self._condition.now = TypesCondition.N16
+        elif checking_symbol.is_value_letter(self._reader.selected_symbol):
+            self._condition.now = TypesCondition.ER
+        else:
+            self._token.writing_token_to_table(TypesOfTokenTables.NUMBERS)
+            self._token.writing_to_token_file(3, self._token.z)
+            self._condition.now = TypesCondition.H
