4b9b7e9474e343af86d080f6e1517bc075bc7c4a
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-16 09:20:07 +0300

    Доработано состояние conditionN10

--- conditions/conditionn10.py
+++ conditions/conditionn10.py
@@ -2,9 +2,12 @@ from typing import List
 
 from buffer import Buffer
 from characterreader import CharacterReader
+from checkingsymbol import CheckingSymbol
 from conditions.condition import Condition
 from conditions.conditionparent import ConditionParent
+from conditions.typescondition import TypesCondition
 from myutils import reading_next_character
+from tokens.typesoftokentables import TypesOfTokenTables
 from tokens.workingwithtoken import WorkingWithToken
 from transitions.transitionparent import TransitionParent
 
@@ -14,8 +17,29 @@ class ConditionN10(ConditionParent):
         super().__init__(reader, buffer, token, condition)
 
     def action(self, transitions: List[TransitionParent]) -> None:
+        checking_symbol = CheckingSymbol()
         self.__cleaning_from_code()
-        super(ConditionN10, self).action(transitions)
+        if self._reader.selected_symbol in ['A', 'a', 'B', 'b', 'C', 'c', 'F', 'f']:
+            self._condition.now = TypesCondition.N16
+        elif self._reader.selected_symbol in ['E', 'e']:
+            reading_next_character(self._buffer, self._reader)
+            self._condition.now = TypesCondition.E11
+        elif self._reader.selected_symbol in ['H', 'h']:
+            self._reader.trip_first_character()
+            self._condition.now = TypesCondition.HX
+        elif self._reader.selected_symbol == '.':
+            reading_next_character(self._buffer, self._reader)
+            self._condition.now = TypesCondition.P1
+        elif self._reader.selected_symbol in ['D', 'd']:
+            reading_next_character(self._buffer, self._reader)
+            self._condition.now = TypesCondition.D
+        elif checking_symbol.is_value_letter(self._reader.selected_symbol):
+            self._condition.now = TypesCondition.ER
+        else:
+            self._token.writing_token_to_table(TypesOfTokenTables.NUMBERS)
+            self._token.writing_to_token_file(2, self._token.z)
+            self._condition.now = TypesCondition.H
 
     def __cleaning_from_code(self) -> None:
-        reading_next_character(self._buffer, self._reader)
+        while self._reader.selected_symbol in ['8', '9']:
+            reading_next_character(self._buffer, self._reader)
