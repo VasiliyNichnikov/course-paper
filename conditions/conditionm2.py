771e38003764b4fc2981784066de5f947858952e
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-16 09:18:36 +0300

    Доработано состояние conditionM2

--- conditions/conditionm2.py
+++ conditions/conditionm2.py
@@ -4,6 +4,7 @@ from buffer import Buffer
 from characterreader import CharacterReader
 from conditions.condition import Condition
 from conditions.conditionparent import ConditionParent
+from conditions.typescondition import TypesCondition
 from tokens.workingwithtoken import WorkingWithToken
 from transitions.transitionparent import TransitionParent
 
@@ -13,4 +14,9 @@ class ConditionM2(ConditionParent):
         super().__init__(reader, buffer, token, condition)
 
     def action(self, transitions: List[TransitionParent]) -> None:
-        super(ConditionM2, self).action(transitions)
+        if self._reader.selected_symbol == '=':
+            self._reader.trip_first_character()
+            self._token.writing_to_token_file(1, 21)
+        else:
+            self._token.writing_to_token_file(1, 18)
+        self._condition.now = TypesCondition.H
