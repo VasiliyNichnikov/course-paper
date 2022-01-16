60b8e664844f325f07f59021f8ac06282affe77a
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-16 09:07:03 +0300

    Доработано состояние conditionB

--- conditions/conditionb.py
+++ conditions/conditionb.py
@@ -2,8 +2,11 @@ from typing import List
 
 from buffer import Buffer
 from characterreader import CharacterReader
+from checkingsymbol import CheckingSymbol
 from conditions.condition import Condition
 from conditions.conditionparent import ConditionParent
+from conditions.typescondition import TypesCondition
+from myutils import converting_numbers_writing_to_tokens
 from tokens.workingwithtoken import WorkingWithToken
 from transitions.transitionparent import TransitionParent
 
@@ -13,4 +16,14 @@ class ConditionB(ConditionParent):
         super().__init__(reader, buffer, token, condition)
 
     def action(self, transitions: List[TransitionParent]) -> None:
-        super(ConditionB, self).action(transitions)
+        checking_symbol = CheckingSymbol()
+        if checking_symbol.check_hex(self._reader.selected_symbol):
+            self._condition.now = TypesCondition.N16
+        elif self._reader.selected_symbol in ['H', 'h']:
+            self._reader.trip_first_character()
+            self._condition.now = TypesCondition.HX
+        elif checking_symbol.is_value_letter(self._reader.selected_symbol):
+            self._condition.now = TypesCondition.ER
+        else:
+            converting_numbers_writing_to_tokens(self._buffer, self._token, 2)
+            self._condition.now = TypesCondition.H
