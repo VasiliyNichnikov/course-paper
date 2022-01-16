729fca36a736462039ae221e8d0a8929847a7d54
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-16 09:29:37 +0300

    Добавлены новые функции в файл myutils.py

--- myutils.py
+++ myutils.py
@@ -1,8 +1,11 @@
 from buffer import Buffer
 from characterreader import CharacterReader
+from tokens.typesoftokentables import TypesOfTokenTables
+from tokens.workingwithtoken import WorkingWithToken
+from workingwithnumber import WorkingWithNumber
 
 
-def reading_next_character_and_cleaning_buffer(buffer: Buffer, reader: CharacterReader) -> None:
+def cleaning_buffer_reading_next_character(buffer: Buffer, reader: CharacterReader) -> None:
     buffer.clear()
     buffer.add(reader.selected_symbol)
     reader.trip_first_character()
@@ -11,3 +14,23 @@ def reading_next_character_and_cleaning_buffer(buffer: Buffer, reader: Character
 def reading_next_character(buffer: Buffer, reader: CharacterReader) -> None:
     buffer.add(reader.selected_symbol)
     reader.trip_first_character()
+
+
+def converting_numbers_writing_to_tokens(buffer: Buffer, token: WorkingWithToken, base: int) -> None:
+    actions_numbers = WorkingWithNumber()
+    number: str = buffer.get_combined_characters()
+    new_number = actions_numbers.conversion_from_number_system_to_decimal(number, base)
+    buffer.clear()
+    buffer.add(new_number)
+    token.writing_token_to_table(TypesOfTokenTables.NUMBERS)
+    token.writing_to_token_file(2, token.z)
+
+
+def converting_string_to_numbers_writing_to_tokens(buffer: Buffer, token: WorkingWithToken) -> None:
+    actions_numbers = WorkingWithNumber()
+    number: str = buffer.get_combined_characters()
+    new_number = actions_numbers.converting_from_string_to_decimal_form(number)
+    buffer.clear()
+    buffer.add(new_number)
+    token.writing_token_to_table(TypesOfTokenTables.NUMBERS)
+    token.writing_to_token_file(2, token.z)
