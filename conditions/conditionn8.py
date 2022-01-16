596dcebd1cd45e45b9f1ee563a2cb4dc5efe3d99
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-16 09:19:38 +0300

    Доработано состояние conditionN8

--- conditions/conditionn8.py
+++ conditions/conditionn8.py
@@ -2,8 +2,10 @@ from typing import List
 
 from buffer import Buffer
 from characterreader import CharacterReader
+from checkingsymbol import CheckingSymbol
 from conditions.condition import Condition
 from conditions.conditionparent import ConditionParent
+from conditions.typescondition import TypesCondition
 from myutils import reading_next_character
 from tokens.workingwithtoken import WorkingWithToken
 from transitions.transitionparent import TransitionParent
@@ -14,8 +16,32 @@ class ConditionN8(ConditionParent):
         super().__init__(reader, buffer, token, condition)
 
     def action(self, transitions: List[TransitionParent]) -> None:
+        checking_symbol = CheckingSymbol()
         self.__cleaning_from_code()
-        super(ConditionN8, self).action(transitions)
+        if self._reader.selected_symbol in ['8', '9']:
+            self._condition.now = TypesCondition.N10
+        elif self._reader.selected_symbol in ['A', 'a', 'B', 'b', 'C', 'c', 'F', 'f']:
+            self._condition.now = TypesCondition.N16
+        elif self._reader.selected_symbol in ['E', 'e']:
+            reading_next_character(self._buffer, self._reader)
+            self._condition.now = TypesCondition.E11
+        elif self._reader.selected_symbol in ['D', 'd']:
+            reading_next_character(self._buffer, self._reader)
+            self._condition.now = TypesCondition.D
+        elif self._reader.selected_symbol in ['H', 'h']:
+            self._reader.trip_first_character()
+            self._condition.now = TypesCondition.HX
+        elif self._reader.selected_symbol == '.':
+            reading_next_character(self._buffer, self._reader)
+            self._condition.now = TypesCondition.P1
+        elif self._reader.selected_symbol in ['O', 'o']:
+            self._buffer.add(self._reader.selected_symbol)
+            self._condition.now = TypesCondition.O
+        elif checking_symbol.is_value_letter(self._reader.selected_symbol):
+            self._condition.now = TypesCondition.ER
+        else:
+            self._condition.now = TypesCondition.N10
 
     def __cleaning_from_code(self) -> None:
-        reading_next_character(self._buffer, self._reader)
+        while self._reader.selected_symbol in ['2', '3', '4', '5', '6', '7']:
+            reading_next_character(self._buffer, self._reader)
