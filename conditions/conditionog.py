6d89fb8b18da880667c69d4ec9e6a5efde290faa
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-16 09:21:04 +0300

    Доработано состояние conditionOG

--- conditions/conditionog.py
+++ conditions/conditionog.py
@@ -4,6 +4,7 @@ from buffer import Buffer
 from characterreader import CharacterReader
 from conditions.condition import Condition
 from conditions.conditionparent import ConditionParent
+from conditions.typescondition import TypesCondition
 from tokens.typesoftokentables import TypesOfTokenTables
 from tokens.workingwithtoken import WorkingWithToken
 from transitions.transitionparent import TransitionParent
@@ -17,4 +18,10 @@ class ConditionOG(ConditionParent):
         self._buffer.clear()
         self._buffer.add(self._reader.selected_symbol)
         self._token.writing_token_to_table(TypesOfTokenTables.LIMITERS)
-        super(ConditionOG, self).action(transitions)
+
+        if self._token.z != 0:
+            self._reader.trip_first_character()
+            self._token.writing_to_token_file(1, self._token.z)
+            self._condition.now = TypesCondition.H
+        else:
+            self._condition.now = TypesCondition.ER
