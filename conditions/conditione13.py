d513787a20da6002dff3cf2d03bca5810e8b4e92
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-16 09:14:31 +0300

    Доработано состояние conditionE13

--- conditions/conditione13.py
+++ conditions/conditione13.py
@@ -5,7 +5,8 @@ from characterreader import CharacterReader
 from checkingsymbol import CheckingSymbol
 from conditions.condition import Condition
 from conditions.conditionparent import ConditionParent
-from myutils import reading_next_character
+from conditions.typescondition import TypesCondition
+from myutils import reading_next_character, converting_string_to_numbers_writing_to_tokens
 from tokens.workingwithtoken import WorkingWithToken
 from transitions.transitionparent import TransitionParent
 
@@ -15,10 +16,14 @@ class ConditionE13(ConditionParent):
         super().__init__(reader, buffer, token, condition)
 
     def action(self, transitions: List[TransitionParent]) -> None:
-        self.__cleaning_from_code()
-        super(ConditionE13, self).action(transitions)
-
-    def __cleaning_from_code(self) -> None:
         checking_symbol = CheckingSymbol()
+        self.__cleaning_from_code(checking_symbol)
+        if checking_symbol.is_value_letter(self._reader.selected_symbol) or self._reader.selected_symbol == '.':
+            self._condition.now = TypesCondition.ER
+        else:
+            converting_string_to_numbers_writing_to_tokens(self._buffer, self._token)
+            self._condition.now = TypesCondition.H
+
+    def __cleaning_from_code(self, checking_symbol: CheckingSymbol) -> None:
         while checking_symbol.is_value_number(self._reader.selected_symbol):
             reading_next_character(self._buffer, self._reader)
