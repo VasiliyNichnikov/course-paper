d98d85f3cad2650aa11b20f56b473a5aa9803142
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-16 09:21:20 +0300

    Доработано состояние conditionP1

--- conditions/conditionp1.py
+++ conditions/conditionp1.py
@@ -2,8 +2,11 @@ from typing import List
 
 from buffer import Buffer
 from characterreader import CharacterReader
+from checkingsymbol import CheckingSymbol
 from conditions.condition import Condition
 from conditions.conditionparent import ConditionParent
+from conditions.typescondition import TypesCondition
+from myutils import reading_next_character
 from tokens.workingwithtoken import WorkingWithToken
 from transitions.transitionparent import TransitionParent
 
@@ -13,4 +16,9 @@ class ConditionP1(ConditionParent):
         super().__init__(reader, buffer, token, condition)
 
     def action(self, transitions: List[TransitionParent]) -> None:
-        super(ConditionP1, self).action(transitions)
+        checking_symbol = CheckingSymbol()
+        if checking_symbol.is_value_number(self._reader.selected_symbol):
+            reading_next_character(self._buffer, self._reader)
+            self._condition.now = TypesCondition.P2
+        else:
+            self._condition.now = TypesCondition.ER
