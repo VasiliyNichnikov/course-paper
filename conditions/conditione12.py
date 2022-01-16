69a484441431b3a96f49c2963bb5864fc905569b
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-16 09:11:04 +0300

    Доработано состояние conditionE12

--- conditions/conditione12.py
+++ conditions/conditione12.py
@@ -5,7 +5,8 @@ from characterreader import CharacterReader
 from checkingsymbol import CheckingSymbol
 from conditions.condition import Condition
 from conditions.conditionparent import ConditionParent
-from myutils import reading_next_character
+from conditions.typescondition import TypesCondition
+from myutils import reading_next_character, converting_string_to_numbers_writing_to_tokens
 from tokens.workingwithtoken import WorkingWithToken
 from transitions.transitionparent import TransitionParent
 
@@ -15,10 +16,20 @@ class ConditionE12(ConditionParent):
         super().__init__(reader, buffer, token, condition)
 
     def action(self, transitions: List[TransitionParent]) -> None:
-        self.__cleaning_from_code()
-        super(ConditionE12, self).action(transitions)
-
-    def __cleaning_from_code(self) -> None:
         checking_symbol = CheckingSymbol()
+
+        self.__cleaning_from_code(checking_symbol)
+        if checking_symbol.check_hex(self._reader.selected_symbol):
+            self._condition.now = TypesCondition.N16
+        elif self._reader.selected_symbol in ['H', 'h']:
+            self._reader.trip_first_character()
+            self._condition.now = TypesCondition.HX
+        elif checking_symbol.is_value_letter(self._reader.selected_symbol):
+            self._condition.now = TypesCondition.ER
+        else:
+            converting_string_to_numbers_writing_to_tokens(self._buffer, self._token)
+            self._condition.now = TypesCondition.H
+
+    def __cleaning_from_code(self, checking_symbol: CheckingSymbol) -> None:
         while checking_symbol.is_value_number(self._reader.selected_symbol):
             reading_next_character(self._buffer, self._reader)
