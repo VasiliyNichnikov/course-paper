937fe0f169d328fbfcf9a6d2cb88c83e5b84dfcc
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-16 09:14:48 +0300

    Доработано состояние conditionE21

--- conditions/conditione21.py
+++ conditions/conditione21.py
@@ -2,8 +2,11 @@ from typing import List
 
 from buffer import Buffer
 from characterreader import CharacterReader
+from checkingsymbol import CheckingSymbol
 from conditions.condition import Condition
 from conditions.conditionparent import ConditionParent
+from conditions.typescondition import TypesCondition
+from myutils import reading_next_character
 from tokens.workingwithtoken import WorkingWithToken
 from transitions.transitionparent import TransitionParent
 
@@ -13,4 +16,11 @@ class ConditionE21(ConditionParent):
         super().__init__(reader, buffer, token, condition)
 
     def action(self, transitions: List[TransitionParent]) -> None:
-        super(ConditionE21, self).action(transitions)
+        checking_symbol = CheckingSymbol()
+        if self._reader.selected_symbol in ['+', '-']:
+            reading_next_character(self._buffer, self._reader)
+            self._condition.now = TypesCondition.ZN
+        elif checking_symbol.is_value_number(self._reader.selected_symbol):
+            self._condition.now = TypesCondition.E22
+        else:
+            self._condition.now = TypesCondition.ER
