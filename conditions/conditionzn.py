f8afc322402acbd2e265eef1be633ac4cb760db0
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-16 09:42:18 +0300

    Доработано состояние conditionZN

--- conditions/conditionzn.py
+++ conditions/conditionzn.py
@@ -1,11 +1,11 @@
-from typing import List
-
 from buffer import Buffer
 from characterreader import CharacterReader
-from conditions.conditionparent import ConditionParent
+from checkingsymbol import CheckingSymbol
 from conditions.condition import Condition
+from conditions.conditionparent import ConditionParent
+from conditions.typescondition import TypesCondition
+from myutils import reading_next_character
 from tokens.workingwithtoken import WorkingWithToken
-from transitions.transitionparent import TransitionParent
 
 
 class ConditionZN(ConditionParent):
@@ -13,4 +13,9 @@ class ConditionZN(ConditionParent):
         super().__init__(reader, buffer, token, condition)
 
     def action(self) -> None:
-        super(ConditionZN, self).action()
+        checking_symbol = CheckingSymbol()
+        if checking_symbol.is_value_number(self._reader.selected_symbol):
+            reading_next_character(self._buffer, self._reader)
+            self._condition = TypesCondition.E13
+        else:
+            self._condition = TypesCondition.ER
